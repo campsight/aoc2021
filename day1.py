6564f81c993f9f6f440e1dfc44eba54e3fccf81b
campsight <ir.j.camp@gmail.com>
2021-12-01 06:12:51 +0100

    Getting ready ;-)

--- day1.py
+++ day1.py
@@ -1,3 +1,19 @@
 from aoc_help import *
-lines = read_file('data/input-day1t.txt')
+lines = read_file('data/input-day1.txt')
+data = [int(x) for x in lines]
 
+# part 1
+count = 0
+for i in range(1, len(data)):
+    count += data[i] > data[i-1]
+
+print(f"Part 1: {count}")
+
+# part 2
+count = 0
+for i in range(1, len(data)-2):
+    sum1 = sum(data[(i-1):(i+2)])
+    sum2 = sum(data[i:(i+3)])
+    count += sum2 > sum1
+
+print(f"Part 2: {count}")
